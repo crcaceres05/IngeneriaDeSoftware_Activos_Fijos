5636027b26f8f782d03d620d9315bb1e8992ec87
Renato Caceres <36979690+crcaceres05@users.noreply.github.com>
2022-04-07 10:05:15 -0600

    Se soluciono problema con la opcion editar, funciona correctamente

--- Registros/editar.php
+++ Registros/editar.php
@@ -9,7 +9,7 @@
     include_once 'model/conexion.php';
     $idActivo = $_GET['idActivo'];
 
-    $sentencia = $bd->prepare("select * from Activo");
+    $sentencia = $bd->prepare("select * from Activo where idActivo = ?;");
     $sentencia_usuarios = $bd->query("select * from Empleado");
     $sentencia->execute([$idActivo]);
     $activo = $sentencia->fetch(PDO::FETCH_OBJ);
@@ -25,32 +25,26 @@
                 <div class="card-header">
                     Editar Activos:
                 </div>
-
-                <?php 
-                                $array1 = json_decode(json_encode($activo), true);
-                                foreach($activo as $dato){ 
-                            ?>
-
                 <form class="p-4" method="POST" action="editarProceso.php">
                 <div class="mb-3">
                         <label class="form-label">No_Serial: </label>
                         <input type="text" class="form-control" name="txtNo_Serial" autofocus required
-                        value="<?php echo $dato->No_Serial; ?>">
+                        value="<?php echo $activo->No_Serial; ?>">
                     </div>
                     <div class="mb-3">
                         <label class="form-label">Descripcion: </label>
                         <input type="text" class="form-control" name="txtDescripcion" required 
-                        value="<?php echo $dato->Descripcion; ?>">
+                        value="<?php echo $activo->Descripcion; ?>">
                     </div>
                     <div class="mb-3">
                         <label class="form-label">Valor: </label>
                         <input type="number" class="form-control" name="txtValor" autofocus required
-                        value="<?php echo $dato->Valor; ?>">
+                        value="<?php echo $activo->Valor; ?>">
                     </div>
                     <div class="mb-3">
                         <label class="form-label">Fecha Compra: </label>
                         <input type="text" class="form-control" name="txtFechaCompra" autofocus required
-                        value="<?php echo $dato->FechaCompra; ?>">
+                        value="<?php echo $activo->FechaCompra; ?>">
                     </div>
                      <div class="mb-3">
                          <label class ="form-label">Usuario:</label>
@@ -67,12 +61,9 @@
                          </select>
                      </div>         
                     <div class="d-grid">
-                        <input type="hidden" name="idActivo" value="<?php echo $dato->idActivo; ?>">
+                        <input type="hidden" name="idActivo" value="<?php echo $activo->idActivo; ?>">
                         <input type="submit" class="btn btn-primary" value="Editar">
                     </div>
-                    <?php 
-                                }
-                            ?>
                 </form>
             </div>
         </div>
